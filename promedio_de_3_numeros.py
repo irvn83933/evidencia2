0a760832af280425585699f53a7e9923ae0f8937
irvn83933 <irvinmagallanes231@gmail.com>
2026-07-22 17:45:53 -0600

    Add files via upload

---
+++ promedio_de_3_numeros.py
@@ -0,0 +1,6 @@
+#promedio de 3 numeros
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+num3 = float(input("Ingrese el tercer número: "))
+promedio = (num1 + num2 + num3) / 3
+print("El promedio de los tres números es:", promedio)
