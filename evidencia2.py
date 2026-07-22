8c7a648b4da0b07eeb9b1ddb734d7addd5a362c9
irvn83933 <irvinmagallanes231@gmail.com>
2026-07-22 17:37:32 -0600

    Add files via upload

---
+++ evidencia2.py
@@ -0,0 +1,5 @@
+#calcular el area de un recangulo
+base = float(input("Ingrese la base del rectángulo: "))
+altura = float(input("Ingrese la altura del rectángulo: "))
+area = base * altura
+print("El área del rectángulo es:", area)   
